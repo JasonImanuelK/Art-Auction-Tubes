4a7aaa7f193895e457637cf6bc304f4a5c201e46
jordaninformatics <jordaninformatics@gmail.com>
2022-04-08 15:47:45 +0700

    menambah fungsi mengambil marketList dari nama

--- controller/controllerMarket.go
+++ controller/controllerMarket.go
@@ -41,3 +41,36 @@ func GetMarketListById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(MarketResponse)
 }
+
+func GetMarketListByName(w http.ResponseWriter, r *http.Request) {
+	db := connect()
+	defer db.Close()
+
+	err := r.ParseForm()
+	if err != nil {
+		return
+	}
+	vars := mux.Vars(r)
+	marketId := vars["marketId"]
+
+	rows, _ := db.Query("SELECT marketlist.ID, marketlist.StartingDate, marketlist.Deadline, marketlist.StartingBid, marketlist.BuyoutBid, marketlist.DatePosted, marketlist.ImageId, marketlist.Status FROM marketlist JOIN gambar WHERE gambar.title LIKE '%?%'", marketId)
+	var MarketResponse model.MarketResponse
+	var data model.Market
+
+	for rows.Next() {
+		if err := rows.Scan(&data.ID, &data.StartingDate, &data.Deadline, &data.StartingBid, &data.BuyoutBid, &data.DatePosted, &data.ImageId, &data.Status); err != nil {
+			log.Println(err)
+			MarketResponse.Status = 500
+			MarketResponse.Message = "internal error"
+			json.NewEncoder(w).Encode(MarketResponse)
+			return
+		} else {
+			MarketResponse.Data = append(MarketResponse.Data, data)
+			MarketResponse.Status = 200
+			MarketResponse.Message = "Success"
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(MarketResponse)
+}
