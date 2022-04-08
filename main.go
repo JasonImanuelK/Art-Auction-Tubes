2b5ee449950fe28d928af9b1518c9c62f2c63f7a
JasonImanuelK <jasonkasman@gmail.com>
2022-04-08 11:56:31 +0700

    Tambah auth,login, logout, main

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"Tubes/Art-Auction-Tubes/controller"
 
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/login", controller.Login).Methods("PUT")
+	router.HandleFunc("/logout", controller.Logout).Methods("GET")
+
+	http.Handle("/", router)
+	fmt.Println("Connected to port 8080")
+	log.Println("Connected to port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
