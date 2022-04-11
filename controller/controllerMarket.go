59807952d9ea34e28eea0c0b0ac920fa36ba1005
jordaninformatics <jordaninformatics@gmail.com>
2022-04-11 14:34:28 +0700

    menambah get market by date dan reset redis

--- controller/controllerMarket.go
+++ controller/controllerMarket.go
@@ -2,14 +2,103 @@ package controller
 
 import (
 	"Tubes/Art-Auction-Tubes/model"
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 )
+
+func ResetRedis(w http.ResponseWriter, r *http.Request) {
+	var ctx = context.Background()
+	db := connect()
+	defer db.Close()
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	email := r.Form.Get("email")
+
+	err := client.Set(ctx, email, 1, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var response model.GeneralResponse
+
+	if err == nil {
+		response.Status = 200
+		response.Message = "success"
+
+	} else {
+		response.Status = 400
+		response.Message = "internal error"
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func GetMarketListByDate(w http.ResponseWriter, r *http.Request) {
+	var ctx = context.Background()
+	db := connect()
+	defer db.Close()
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	email := r.Form.Get("email")
+
+	current, err1 := client.Get(ctx, email).Result()
+
+	current2, _ := strconv.Atoi(current)
+
+	if err1 != nil {
+
+		err := client.Set(ctx, email, 1, 0).Err()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+
+	rows, _ := db.Query("SELECT *  FROM marketlist WHERE ID >= ?", current2)
+	var MarketResponse model.MarketResponse
+	var data model.Market
+
+	for rows.Next() {
+		if err := rows.Scan(&data.ID, &data.StartingDate, &data.Deadline, &data.StartingBid, &data.BuyoutBid, &data.DatePosted, &data.ImageId, &data.Status); err != nil {
+			log.Println(err)
+			MarketResponse.Status = 500
+			MarketResponse.Message = "internal error"
+			json.NewEncoder(w).Encode(MarketResponse)
+			return
+		} else {
+			MarketResponse.Data = append(MarketResponse.Data, data)
+			MarketResponse.Status = 200
+			MarketResponse.Message = "Success"
+		}
+	}
+
+	err := client.Set(ctx, email, (current2 + 10), 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(MarketResponse)
+}
 
 func GetMarketListById(w http.ResponseWriter, r *http.Request) {
 	db := connect()
