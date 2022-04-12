56e92b7bbfacdba7dfc72e11e94886751bebf155
ivander27 <timotius.ivander04@gmail.com>
2022-04-12 14:44:12 +0700

    add Endpoint(main) & update controllerTransaction

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ func main() {
 	router.HandleFunc("/users", controller.Authenticate(controller.GetUsers, 2)).Methods("GET")
 	router.HandleFunc("/users/{key}", controller.Authenticate(controller.GetUsers, 2)).Methods("GET")
 	router.HandleFunc("/users/{id}", controller.Authenticate(controller.ChangeBanStatus, 1)).Methods("PUT")
+	router.HandleFunc("/bid/{marketId,userId,Etherium}", controller.InsertBid).Methods("PUT")
+	router.HandleFunc("/transaction", controller.GetLatestTransaction).Methods(("GET"))
 
 	http.Handle("/", router)
 	fmt.Println("Connected to port 8080")
